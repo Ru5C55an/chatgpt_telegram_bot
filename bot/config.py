7cda77110a838289274c01083fb1b69e39ce7200
Ruslan Sadykov <ruslan.sn@icloud.com>
2025-12-17 22:16:13 +0300

    Added password

--- bot/config.py
+++ bot/config.py
@@ -23,7 +23,14 @@ enable_message_streaming = config_yaml.get("enable_message_streaming", True)
 return_n_generated_images = config_yaml.get("return_n_generated_images", 1)
 image_size = config_yaml.get("image_size", "512x512")
 n_chat_modes_per_page = config_yaml.get("n_chat_modes_per_page", 5)
-mongodb_uri = f"mongodb://mongo:{config_env['MONGODB_PORT']}"
+mongodb_user = config_env.get("MONGODB_USERNAME")
+mongodb_password = config_env.get("MONGODB_PASSWORD")
+mongodb_port = config_env.get("MONGODB_PORT", "27017")
+
+if mongodb_user and mongodb_password:
+    mongodb_uri = f"mongodb://{mongodb_user}:{mongodb_password}@mongo:{mongodb_port}"
+else:
+    mongodb_uri = f"mongodb://mongo:{mongodb_port}"
 
 # chat_modes
 with open(config_dir / "chat_modes.yml", 'r') as f:
